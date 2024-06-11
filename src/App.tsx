30dad729124647369a0f2394472c4c754757bbf2
David Larrea Goñi <dlarrea@itracasa.es>
2024-06-11 09:24:09 +0200

    Unificamos funcion de creacion elemento y update en lista en una funcion.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import { useReducer, useState } from 'react'
 import './App.css'
 import CreateTaskForm from './components/CreateTaskForm/CreateTaskForm'
 import Layout from './components/Layout/Layout'
 import SearchBar from './components/Search/SearchBar'
 import TasksLists from './components/TaskCard/TaskList'
 import { Task, TaskWithoutId } from './types'
+
+enum TaskAction {
+  Create,
+  Update,
+}
+
+interface TaskReducerAction {
+  action: TaskAction
+  task: Task | TaskWithoutId
+}
 
 function App() {
   const todosList: Task[] = [
@@ -13,27 +23,42 @@ function App() {
     { id: 3, title: 'Prueba título 3', description: 'Descripción 3', completed: false },
   ]
 
-  const [searchText, setSearchText] = useState<string>('')
-  const [tasks, setTasks] = useState(todosList)
-
-  const handleTaskCreated = (task: TaskWithoutId) => {
-    const newTask: Task = {
-      id: tasks.length + 1,
-      ...task,
+  const taskReducer = (state: Task[], action: TaskReducerAction): Task[] => {
+    switch (action.action) {
+      case TaskAction.Create:
+        if ('id' in action.task) {
+          const newTask = action.task as Task
+          return [...state, newTask]
+        } else {
+          const newTask = action.task as TaskWithoutId
+          const nextId = state.length > 0 ? state[state.length - 1].id + 1 : 1
+          const taskWithId = { ...newTask, id: nextId }
+          return [...state, taskWithId]
+        }
+      case TaskAction.Update:
+        return state.map((t) => ('id' in action.task && t.id === action.task.id ? action.task : t))
+      default:
+        return state
     }
-    setTasks([...tasks, newTask])
   }
 
-  const handleTaskChanged = (task: Task) => {
-    const newTasks = tasks.map((t) => (t.id === task.id ? task : t))
-    setTasks(newTasks)
+  const [searchText, setSearchText] = useState<string>('')
+  const [tasks, dispatch] = useReducer(taskReducer, todosList)
+
+  const handleTaskCreated = (task: TaskWithoutId) => {
+    dispatch({ action: TaskAction.Create, task })
+  }
+
+  const handleTaskUpdated = (task: Task) => {
+    dispatch({ action: TaskAction.Update, task })
   }
 
   return (
     <Layout>
       <SearchBar onSearch={(searchText: string) => setSearchText(searchText)} />
       <br />
-      <TasksLists tasks={tasks} filter={searchText} onTaskChanged={handleTaskChanged} />
+      <TasksLists tasks={tasks} filter={searchText} onTaskChanged={handleTaskUpdated} />
+      <br />
       <CreateTaskForm onTaskCreated={handleTaskCreated} />
     </Layout>
   )
